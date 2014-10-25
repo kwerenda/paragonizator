b53f2c59ff0c26a81e489b4b5db04a9193fc418d
bogna <bogna.bb+gh@gmail.com>
2014-10-25 23:15:16 +0200

    updated schema to add product name aliases

--- db/dbschema.sql
+++ db/dbschema.sql
@@ -2,11 +2,12 @@ CREATE EXTENSION IF NOT EXISTS postgis; -- PostGis extension must be installed i
 
 DROP TABLE IF EXISTS products CASCADE;
 CREATE TABLE products (
-  id SERIAL PRIMARY KEY ,
-  gtin BIGINT, -- global trade item number
+  gtin BIGINT PRIMARY KEY , -- global trade item number
   name TEXT NOT NULL
 
 );
+
+
 
 DROP TABLE IF EXISTS shops CASCADE;
 CREATE TABLE shops (
@@ -15,6 +16,15 @@ CREATE TABLE shops (
   location GEOMETRY(POINT,4326) NOT NULL,
   nip text, -- not an ID because sometimes chains have same nip
   UNIQUE (name, location)
+);
+
+DROP TABLE IF EXISTS product_aliases_on_recepits CASCADE;
+CREATE TABLE product_aliases_on_recepits (
+  id SERIAL PRIMARY KEY,
+  product_alias TEXT NOT NULL,
+  product_gtin BIGINT REFERENCES products(gtin),
+  shop_id INTEGER REFERENCES shops(id),
+  UNIQUE (product_alias, product_gtin, shop_id) --two items with same code?
 );
 
 DROP TABLE IF EXISTS users CASCADE;
@@ -45,9 +55,9 @@ INSERT INTO units (name) VALUES
 DROP TABLE IF EXISTS price_entries CASCADE;
 CREATE TABLE price_entries (
   price REAL NOT NULL,
-  product_id BIGINT REFERENCES products(id),
+  product_alias_id INTEGER REFERENCES product_aliases_on_recepits(id),
   receipt_id INTEGER REFERENCES receipts(id),
   quantity REAL DEFAULT 1.00, -- pices or eg. weight: 0.7 kg of banans
   unit text REFERENCES units(name) DEFAULT 'pieces',
-  PRIMARY KEY(product_id, receipt_id)
+  PRIMARY KEY(product_alias_id, receipt_id)
 );
