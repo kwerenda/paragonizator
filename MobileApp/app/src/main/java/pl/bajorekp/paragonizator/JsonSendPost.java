7ed2c1b277499ab7d1068b8456fd018aed26b109
bogna <bogna.bb+gh@gmail.com>
2014-10-25 21:54:32 +0200

    Add custom array adapter with delete buttons (super ugly), GTIN not compulsory anymore in products table

--- MobileApp/app/src/main/java/pl/bajorekp/paragonizator/JsonSendPost.java
+++ MobileApp/app/src/main/java/pl/bajorekp/paragonizator/JsonSendPost.java
@@ -1,154 +1,154 @@
-package pl.bajorekp.paragonizator;
-
-import android.app.Activity;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-
-public class JsonSendPost extends Activity {
-
-    EditText editText;
-    EditText editText2;
-    Button button;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_json_send_post);
-        editText = (EditText) findViewById(R.id.editText);
-        editText2 = (EditText) findViewById(R.id.editText2);
-        button = (Button) findViewById(R.id.button);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.json_send_post, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void sendToServer(View view) {
-        new HttpAsyncTask().execute("http://httpbin.org/post");
-    }
-
-    public static String POST(String url, String data){
-        InputStream inputStream = null;
-        String result = "";
-        try {
-
-            // 1. create HttpClient
-            HttpClient httpclient = new DefaultHttpClient();
-
-            // 2. make POST request to the given URL
-            HttpPost httpPost = new HttpPost(url);
-
-
-            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
-            // ObjectMapper mapper = new ObjectMapper();
-            // json = mapper.writeValueAsString(person);
-
-            // 5. set json to StringEntity
-            StringEntity se = new StringEntity(data);
-
-            // 6. set httpPost Entity
-            httpPost.setEntity(se);
-
-            // 7. Set some headers to inform server about the type of the content
-            httpPost.setHeader("Accept", "application/json");
-            httpPost.setHeader("Content-type", "application/json");
-
-            // 8. Execute POST request to the given URL
-            HttpResponse httpResponse = httpclient.execute(httpPost);
-
-            // 9. receive response as inputStream
-            inputStream = httpResponse.getEntity().getContent();
-
-            // 10. convert inputstream to string
-            if(inputStream != null)
-                result = convertInputStreamToString(inputStream);
-            else
-                result = "Did not work!";
-
-        } catch (Exception e) {
-            Log.d("InputStream", e.getLocalizedMessage());
-        }
-
-
-
-
-        // 11. return result
-        return result;
-    }
-
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += line;
-
-        inputStream.close();
-        return result;
-
-    }
-
-    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... urls) {
-
-            String json = "";
-
-            // 3. build jsonObject
-            JSONObject jsonObject = new JSONObject();
-            String testValue = editText.getText().toString();
-            try {
-                jsonObject.accumulate("TestKey", testValue);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            // 4. convert JSONObject to JSON to String
-            json = jsonObject.toString();
-            return POST(urls[0],json);
-        }
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
-            Toast.makeText(getBaseContext(), "Data Sent!", Toast.LENGTH_LONG).show();
-            editText2.setText(result);
-        }
-    }
-}
+//package pl.bajorekp.paragonizator;
+//
+//import android.app.Activity;
+//import android.os.AsyncTask;
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//import org.apache.http.HttpResponse;
+//import org.apache.http.client.HttpClient;
+//import org.apache.http.client.methods.HttpPost;
+//import org.apache.http.entity.StringEntity;
+//import org.apache.http.impl.client.DefaultHttpClient;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//
+//
+//public class JsonSendPost extends Activity {
+//
+//    EditText editText;
+//    EditText editText2;
+//    Button button;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_json_send_post);
+//        editText = (EditText) findViewById(R.id.editText);
+//        editText2 = (EditText) findViewById(R.id.editText2);
+//        button = (Button) findViewById(R.id.button);
+//    }
+//
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.json_send_post, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    public void sendToServer(View view) {
+//        new HttpAsyncTask().execute("http://httpbin.org/post");
+//    }
+//
+//    public static String POST(String url, String data){
+//        InputStream inputStream = null;
+//        String result = "";
+//        try {
+//
+//            // 1. create HttpClient
+//            HttpClient httpclient = new DefaultHttpClient();
+//
+//            // 2. make POST request to the given URL
+//            HttpPost httpPost = new HttpPost(url);
+//
+//
+//            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+//            // ObjectMapper mapper = new ObjectMapper();
+//            // json = mapper.writeValueAsString(person);
+//
+//            // 5. set json to StringEntity
+//            StringEntity se = new StringEntity(data);
+//
+//            // 6. set httpPost Entity
+//            httpPost.setEntity(se);
+//
+//            // 7. Set some headers to inform server about the type of the content
+//            httpPost.setHeader("Accept", "application/json");
+//            httpPost.setHeader("Content-type", "application/json");
+//
+//            // 8. Execute POST request to the given URL
+//            HttpResponse httpResponse = httpclient.execute(httpPost);
+//
+//            // 9. receive response as inputStream
+//            inputStream = httpResponse.getEntity().getContent();
+//
+//            // 10. convert inputstream to string
+//            if(inputStream != null)
+//                result = convertInputStreamToString(inputStream);
+//            else
+//                result = "Did not work!";
+//
+//        } catch (Exception e) {
+//            Log.d("InputStream", e.getLocalizedMessage());
+//        }
+//
+//
+//
+//
+//        // 11. return result
+//        return result;
+//    }
+//
+//    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+//        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+//        String line = "";
+//        String result = "";
+//        while((line = bufferedReader.readLine()) != null)
+//            result += line;
+//
+//        inputStream.close();
+//        return result;
+//
+//    }
+//
+//    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+//        @Override
+//        protected String doInBackground(String... urls) {
+//
+//            String json = "";
+//
+//            // 3. build jsonObject
+//            JSONObject jsonObject = new JSONObject();
+//            String testValue = editText.getText().toString();
+//            try {
+//                jsonObject.accumulate("TestKey", testValue);
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//            // 4. convert JSONObject to JSON to String
+//            json = jsonObject.toString();
+//            return POST(urls[0],json);
+//        }
+//        // onPostExecute displays the results of the AsyncTask.
+//        @Override
+//        protected void onPostExecute(String result) {
+//            Toast.makeText(getBaseContext(), "Data Sent!", Toast.LENGTH_LONG).show();
+//            editText2.setText(result);
+//        }
+//    }
+//}
