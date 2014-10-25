145363c98785eb01b0f64f94ab457bfc67f1766f
bajorekp <bajorekp@gmail.com>
2014-10-25 07:11:20 +0200

    Add creatin list

--- androidApp/app/src/main/java/pl/bajorekp/paragonizator/CreateList.java
+++ androidApp/app/src/main/java/pl/bajorekp/paragonizator/CreateList.java
@@ -1,12 +1,18 @@
 package pl.bajorekp.paragonizator;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 
-public class AddNewProducts extends Activity {
+public class CreateList extends Activity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +36,23 @@ public class AddNewProducts extends Activity {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.take_photo_of_receipt) {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+            return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView mImageView = (ImageView) findViewById(R.id.imageView);
+            mImageView.setImageBitmap(imageBitmap);
+        }
     }
 }
