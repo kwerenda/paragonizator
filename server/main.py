cff3075b430598d6abe67300260aadef58ae2fb3
Igor Jurkowski <p4r4noj4@gmail.com>
2014-10-26 09:41:08 +0100

    add template folder for app

--- server/main.py
+++ server/main.py
@@ -1,9 +1,13 @@
 import json
-from flask import Flask, make_response
+from flask import Flask, make_response, render_template
 from flask.ext import restful
 from server.database.database import set_up_db
 
-app = Flask(__name__)
+import os
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+
+app = Flask(__name__, template_folder=tmpl_dir)
 app.config.from_object('config')
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://mobileapp:agh@172.27.0.14:5432/paragonizator"
 set_up_db(app)
@@ -22,12 +26,14 @@ api.add_resource(Stats, "/api/stats")
 
 @app.route("/")
 def index():
-    return make_response(open('templates/index.html').read())
+    return render_template('index.html')
 
 @api.representation('application/json')
 def make_json(data, code, headers=None):
     resp = make_response(json.dumps(data), code)
     resp.headers.extend(headers or {})
     return resp
+
+
 
 app.run(host= '0.0.0.0')
